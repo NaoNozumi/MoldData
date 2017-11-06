4b117cbaa5059809876c7e74be8f2f90fe40cbea
Nao Nozumi <NaowizCoa@gmail.com>
2017-11-07 02:32:12 +0900

    製造番号処理追加

--- MoldData/db/db.js
+++ MoldData/db/db.js
@@ -9,17 +9,33 @@
 var xhr = new XMLHttpRequest();
 var MachineList = new Array();
 
+// 製造No.リスト読込
 xhr.onreadystatechange = function() {
-	if (xhr.readyState == 4 && xhr.status == 200) {
-		var i, j, k, l;
-		var result = xhr.responseText.split("\r\n");
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var i, j, k, l;
+        var result = xhr.responseText.split("\r\n");
 
-		for (i = 0; i < result.length; i++) {
-			MachineList[i] = result[i].split("\t");
-		}
-		xhr.abort(); // リクエストを中止
-	}
+        for (i = 0; i < result.length; i++) {
+            MachineList[i] = result[i].split("\t");
+        }
+        xhr.abort(); // リクエストを中止
+    }
 }
 
 xhr.open("GET", "./db/MachineList.txt", true); // ファイルを非同期通信でオープンする
 xhr.send(null); // リクエストを発行
+
+/* =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+/
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- */
+function inputSerial() {
+    var i, j, k, l;
+    var value = document.serialForm.serialNum.value;
+    var regex = /^¥d{4}$/;
+
+    console.log(regex.test(value));
+
+    for (i = 0; i < MachineList.length; i++) {
+        if (MachineList[i][1] == value) alert(MachineList[i][2]+MachineList[i][3]);
+    }
+}
