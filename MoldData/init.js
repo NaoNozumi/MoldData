42e81c7b9ddffcc297b3cd1b25871f3b16a6de96
Nao Nozumi <NaowizCoa@gmail.com>
2017-11-11 18:49:56 +0900

    製造番号検索

--- MoldData/init.js
+++ MoldData/init.js
@@ -12,19 +12,19 @@
 
 // 点火率画面情報
 // ハーフヒータも1行・1列として数えること
-const RATIO_UP_ROW = 9; // 上ヒータ行数(標準:9)
-const RATIO_UP_COL = 19; // 上ヒータ列数(標準:19)
-const RATIO_CORNER = 2; // U字ヒータ数(標準:2)
-const RATIO_DOWN_ROW = 9; // 下ヒータ行数(標準:9)
-const RATIO_DOWN_COL = 19; // 下ヒータ列数(標準:19)
-const FLOW = 0; // 0:正規勝手/1:反対勝手(使い道無かった)
-const CLS_PEHEATING_NUM = 30; // CLSヒータ予熱温度点数
-const CLS_OPERATION_NUM = 30; // CLSヒータ運転温度点数
+let RATIO_UP_ROW = 9; // 上ヒータ行数(標準:9)
+let RATIO_UP_COL = 19; // 上ヒータ列数(標準:19)
+let RATIO_CORNER = 2; // U字ヒータ数(標準:2)
+let RATIO_DOWN_ROW = 9; // 下ヒータ行数(標準:9)
+let RATIO_DOWN_COL = 19; // 下ヒータ列数(標準:19)
+let FLOW = 0; // 0:正規勝手/1:反対勝手(使い道無かった)
+let CLS_PEHEATING_NUM = 30; // CLSヒータ予熱温度点数
+let CLS_OPERATION_NUM = 30; // CLSヒータ運転温度点数
 
 // 型データ構成
 // 「0」始まりとします。(W0~2048点)
 // UPPER_HEATER_ADD行列の最大値+1
-const RATIO_U_S = 171; // U字ヒータ開始アドレス(標準:171)
+let RATIO_U_S = 171; // U字ヒータ開始アドレス(標準:171)
 
 // 点火率画面テーブルの番号
 // 点火率画面(GOTそのもの)で**行**列を表している番号
@@ -37,7 +37,7 @@ const RATIO_U_S = 171; // U字ヒータ開始アドレス(標準:171)
 □ □ □ □ □ □ □ □ □ 1
 9 8 7 6 5 4 3 2 1
 */
-const RATIO_UP_ROW_HEADER = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1); // 上の行(要素数はRATIO_UP_ROWの内容と同じ)
-const RATIO_UP_COL_HEADER = new Array(19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1); // 上の列(要素数はRATIO_UP_COLの内容と同じ)
-const RATIO_DOWN_ROW_HEADER = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1); // 下の行(要素数はRATIO_DOWN_ROWの内容と同じ)
-const RATIO_DOWN_COL_HEADER = new Array(19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1); // 下の列(要素数はRATIO_DOWN_COLの内容と同じ)
+let RATIO_UP_ROW_HEADER = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1); // 上の行(要素数はRATIO_UP_ROWの内容と同じ)
+let RATIO_UP_COL_HEADER = new Array(19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1); // 上の列(要素数はRATIO_UP_COLの内容と同じ)
+let RATIO_DOWN_ROW_HEADER = new Array(9, 8, 7, 6, 5, 4, 3, 2, 1); // 下の行(要素数はRATIO_DOWN_ROWの内容と同じ)
+let RATIO_DOWN_COL_HEADER = new Array(19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1); // 下の列(要素数はRATIO_DOWN_COLの内容と同じ)
